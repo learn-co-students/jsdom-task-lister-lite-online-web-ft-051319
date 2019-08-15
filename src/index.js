1bf256774b5a113e83f02f5741b1c6375358eef8
Brian Velez <bvez94@gmail.com>
2019-08-15 14:23:36 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submitForm = document.querySelector("form#create-task-form");
+  submitForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(event.innerHTML);
+    let content = document.querySelector("input#new-task-description");
+    let taskList = document.querySelector("ul#tasks");
+    let list = document.createElement("li");
+    list.textContent = content.value;
+    taskList.appendChild(list);
+  });
 });
