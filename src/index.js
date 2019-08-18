61fd33d4f6bb630af6c7470a2d8812dbaf5754dc
github username <github email address>
2019-08-18 18:16:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
+
+  const submit = document.querySelector('input[type="submit"');
+
+  submit.addEventListener("click", add_item);
+
+  function add_item(e){
+    e.preventDefault();
+    let new_item = document.querySelector("#new-task-description");
+    const list_container = document.querySelector("#tasks");
+    let li = document.createElement('li');
+    li.textContent = new_item.value
+    list_container.appendChild(li);
+  }
 });
