e6724941f94d2f352bc2b96b263b0dcc094fa75d
wkpescherine <wkpescherine@gmail.com>
2019-08-19 11:14:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+var newNode= " ";
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let button = document.getElementById("button")
+button.addEventListener(`click`, getInput)
+
+function getInput(e){
+    let response = document.getElementById("new-task-description").value;
+    document.getElementById("tasks").innerHTML += `<li>${response}</li>`;
+    alert(response)
+    e.preventDefault()
+}
+
+
