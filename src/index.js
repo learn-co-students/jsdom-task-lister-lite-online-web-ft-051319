fe7216090b46aa6c93aef81010a1d6bb9c440189
jenniferbusca <jennifer.busca@gmail.com>
2019-08-20 14:57:11 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  let createTaskForm = document.getElementById("create-task-form")
+  let newTaskDescription = document.getElementById("new-task-description")
+
+  createTaskForm.addEventListener("submit", createNewTask);
+  });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
